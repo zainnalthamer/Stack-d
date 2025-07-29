084a86a867a03f2648765fd10b1674c17152e9a1
Zainab <zainnalthamer@gmail.com>
2025-07-29 16:19:44 +0300

    Fixed redirection issue after login

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -6,6 +6,16 @@ const User = require('../models/User');
 const Game = require('../models/Game');
 
 // ADDING ROUTES
+// displaying user dashboard
+router.get('/dashboard', (req, res) => {
+    if(!req.session.user) {
+        return res.redirect('/auth/login');
+    }
+
+    res.render('games/dashboard', {
+        user:req.session.user
+    });
+});
 
 // exporting the router
 module.exports = router;
