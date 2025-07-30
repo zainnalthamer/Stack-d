64b578ed0ac74bfed417e1217c401fe94af02352
Zainab <zainnalthamer@gmail.com>
2025-07-30 13:30:51 +0300

    Added edit profile form

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -8,5 +8,20 @@ const Game = require('../models/Game');
 // importing middleware
 const isLoggedIn = require('../middleware/isLoggedIn');
 
+// importing bcrypt
+const bcrypt = require('bcrypt');
+
+// viewing profile
+router.get('/profile', isLoggedIn, async (req, res) => {
+   const user = await User.findById(req.session.user._id);
+   res.render('users/profile', {user}); 
+});
+
+// displaying edit profile form
+router.get('/profile/edit', isLoggedIn, async (req, res) => {
+    const user = await User.findById(req.session.user._id);
+    res.render('users/editProfile');
+});
+
 // exporting the router
 module.exports = router;
