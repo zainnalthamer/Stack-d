7472c6097507bfc0abf6c86b9cf63902ca53c212
Zainab <zainnalthamer@gmail.com>
2025-08-04 13:25:42 +0300

    Removed comments

--- routes/game.routes.js
+++ routes/game.routes.js
@@ -12,21 +12,6 @@ const upload = multer({ storage });
 
 // ADDING ROUTES
 // displaying user dashboard (all games)
-// router.get('/dashboard', async (req, res) => {
-//     if(!req.session.user) {
-//         return res.redirect('/auth/login');
-//     }
-
-//     try {
-//         const games = await Game.find({user: req.session.user._id});
-//         const user = await User.findById(req.session.user._id);
-//         res.render('games/dashboard', {games, user});
-//     } catch (error) {
-//         console.log('Error fetching games', error);
-//         res.redirect('/games');
-//     }
-// });
-
 router.get('/dashboard', async (req, res) => {
     if(!req.session.user) {
         return res.redirect('/auth/login');
@@ -77,47 +62,6 @@ router.get('/:id', isLoggedIn, async (req, res) => {
 })
 
 // creating a new game
-// router.post('/', isLoggedIn, upload.single('image'), async (req, res) => {
-//     try {
-//         const { title, platform, genre, releaseYear, developer, status, userRating } = req.body;
-
-//         if (!title || !platform || !genre || !releaseYear || !developer || !status) {
-//             return res.render('games/new', {
-//                 error: 'Please fill in all required fields'
-//             });
-//         }
-
-//         if (userRating && (isNaN(userRating) || userRating < 0 || userRating > 10)) {
-//             return res.render('games/new', {
-//                 error: 'Rating must be a number between 0 and 10.'
-//             });
-//         }
-
-//         let imageUrl = '/images/logo-no-text.png';
-//         if (req.file) {
-//             imageUrl = req.file.path; 
-//         }
-
-//         await Game.create({
-//             title: req.body.title,
-//                 platform: req.body.platform,
-//                 genre: req.body.genre,
-//                 releaseYear: req.body.releaseYear,
-//                 developer: req.body.developer,
-//                 status: req.body.status,
-//                 userRating: req.body.userRating,
-//                 userReview: req.body.userReview,
-//                 notes: req.body.notes,
-//                 image: imageUrl,
-//                 user: req.session.user._id
-//         });
-
-//     res.redirect('/games/dashboard');
-// } catch (error) {
-//     console.error('Error creating game', error);
-//     res.redirect('/games/new');
-// }
-// });
 router.post('/', isLoggedIn, upload.single('image'), async (req, res) => {
     try {
         const { title, platform, genre, releaseYear, developer, status, userRating } = req.body;
@@ -174,48 +118,6 @@ router.get('/:id/edit', isLoggedIn, async (req, res) => {
 });
 
 // editing a game
-// router.put('/:id', isLoggedIn, upload.single('image'), async (req, res) => {
-
-//     const { title, platform, genre, releaseYear, developer, status, userRating } = req.body;
-
-//     const game = await Game.findById(req.params.id);
-
-//     if(!title || !platform || !genre || !releaseYear || !developer || !status) {
-//         return res.render('games/edit', {
-//             error: 'Please fill in all required fields.', game
-//         });
-//     }
-
-//     if(userRating && (isNaN(userRating) || userRating < 0 || userRating > 10)) {
-//         return res.render('games/edit', {
-//             error: 'Rating must be a number between 0 and 10.', game
-//         });
-//     }
-
-//     if(!game || game.user.toString() !== req.session.user._id) {
-//         return res.redirect('/games/dashboard');
-//     }
-
-//     // update image if a new one is uploaded
-//     if(req.file) {
-//         game.image = req.file.path;
-//     }
-
-//     // update other fields
-//     game.title = req.body.title;
-//     game.platform = req.body.platform;
-//     game.genre = req.body.genre;
-//     game.releaseYear = req.body.releaseYear;
-//     game.developer = req.body.developer;
-//     game.status = req.body.status;
-//     game.userRating = req.body.userRating;
-//     game.userReview = req.body.userReview;
-//     game.notes = req.body.notes;
-
-//     await game.save();
-    
-//     res.redirect(`/games/${game._id}`);
-// });
 router.put('/:id', isLoggedIn, upload.single('image'), async (req, res) => {
 
     const { title, platform, genre, releaseYear, developer, status, userRating } = req.body;
